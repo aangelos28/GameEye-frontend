69720ef1004f6b7f42d0c918e6034703259d247b
Angelos Angelopoulos <aangelos28@gmail.com>
2020-07-20 19:20:40 -0400

    Add authentication race condition workaround

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import {DashboardComponent} from "./components/dashboard/dashboard.component";
 
 const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-    { path: 'account', component: AccountComponent, canActivate: [AuthGuard] }
+    { path: 'account', component: AccountComponent, canActivate: [AuthGuard] },
+    { path: '**', redirectTo: 'dashboard' }
 ];
 
 @NgModule({
