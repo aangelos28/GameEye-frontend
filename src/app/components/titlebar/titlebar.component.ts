af6fd0b69f25510878a71920066a8d1432b77dec
Angelos Angelopoulos <aangelos28@gmail.com>
2020-07-21 02:01:35 -0400

    Add titlebar and shadow to navbar and titlebar

---
+++ src/app/components/titlebar/titlebar.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-titlebar',
+    templateUrl: './titlebar.component.html',
+    styleUrls: ['./titlebar.component.scss']
+})
+export class TitlebarComponent implements OnInit {
+
+    @Input() titletext: string;
+    @Input() titleiconfacode: string;
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
