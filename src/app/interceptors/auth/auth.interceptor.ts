6477a913db3ead833b89175af89d08bf15e5e018
Angelos Angelopoulos <aangelos28@gmail.com>
2020-07-22 17:59:44 -0400

    Remove redundant console log

--- src/app/interceptors/auth/auth.interceptor.ts
+++ src/app/interceptors/auth/auth.interceptor.ts
@@ -17,7 +17,6 @@ export class AuthInterceptor implements HttpInterceptor {
 
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
         return this.authService.accessToken$.pipe(switchMap((accessToken: string) => {
-            console.log(accessToken);
             if (accessToken) {
                 const authRequest = request.clone({
                     headers: request.headers.set("Authorization", "Bearer " + accessToken)
