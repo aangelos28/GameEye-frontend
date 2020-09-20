1f1323aadda5a9685eee53acbdb98ffd1a2d3c3b
Angelos Angelopoulos <aangelos28@gmail.com>
2020-09-20 18:30:25 -0400

    Add RedirectDataService local storage persistence

--- src/app/shared/services/redirect-data.service.ts
+++ src/app/shared/services/redirect-data.service.ts
@@ -7,10 +7,27 @@ export class RedirectDataService {
 
     public data: any;
 
+    private localStorageKey = 'redirectData';
+
     constructor() {
     }
 
-    reset(): void {
+    public reset(): void {
         this.data = {};
+        if (this.isPersisted()) {
+            localStorage.removeItem(this.localStorageKey);
+        }
+    }
+
+    public isPersisted(): boolean {
+        return localStorage.hasOwnProperty(this.localStorageKey);
+    }
+
+    public persistToLocalStorage(): void {
+        localStorage.setItem(this.localStorageKey, JSON.stringify(this.data));
+    }
+
+    public readFromLocalStorage(): void {
+        this.data = JSON.parse(localStorage.getItem(this.localStorageKey));
     }
 }
